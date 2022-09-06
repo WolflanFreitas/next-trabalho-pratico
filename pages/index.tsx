5d384855e597be0f2655266addf4fba50b3eb4ba
WolflanFreitas <wolflanf@gmail.com>
2022-09-06 19:27:46 -0300

    Limpeza

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,19 @@ import type { NextPage } from 'next'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Button } from 'react-bootstrap';
-import { useState } from 'react';
 
 const Home: NextPage = () => {
-  const [count, setCount] = useState(10)
+
   return (
     <>
       <Container>
         <Row>
-          <Col sm={8}>{count}</Col>
-          <Col sm={4}>
-            <Button as='a' variant='primary' onClick={() => { setCount(count + 1) }}>
-              Incrementar
-            </Button>
+          <Col sm={8}>
+            Aqui
           </Col>
-        </Row>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
+          <Col sm={4}>
+            Acolá
+          </Col>
         </Row>
       </Container>
     </>
