1e7741a0bb5d38c57c0832b092c4ab06bd192a2a
WolflanFreitas <wolflanf@gmail.com>
2022-09-06 22:39:09 -0300

    Listar Produtos

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useFetch } from '../hook/fetchTyped';
 import { Alert } from 'react-bootstrap';
+import { CircleLoader } from 'react-spinners';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'next/image';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 interface Product {
   id: number;
@@ -20,9 +25,10 @@ interface Product {
 }
 
 const Home: NextPage = () => {
-  const url = 'https://fakestoreapi.com/products?limit=10';
+  const url = 'https://fakestoreapi.com/products?limit=100';
   const { data, error } = useFetch<Product[]>(url);
 
+  //Será chamado quando houver erro ao chamar a api (indisponibilidade ou chamada incorreta)
   if (error) {
     return (
       <Container fluid>
@@ -35,21 +41,49 @@ const Home: NextPage = () => {
     )
   }
 
+  //Ação que será executada enquanto os dados estão sendo carregados da API
   if (!data) {
-    return <p>Carregando...</p>
+    return (
+      <Container fluid>
+        <Row className='justify-content-center align-content-center vh-100'>
+          <Col xl={'auto'}>
+            <CircleLoader color={'fuchsia'} loading={true} size={150} />
+          </Col>
+        </Row>
+      </Container>
+    )
   }
-
 
   return (
     <>
-      <Container>
+      <Container fluid>
         <Row>
-          <Col sm={8}>
-
-          </Col>
-          <Col sm={4}>
-            Acolá
-          </Col>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home">
+                <Image src={'/cpu.png'} alt="logo" width={30} height={30} className={"d-inline-block align-top"} />
+                <span className={"d-inline-block align-top"}>Lojão do Hardware</span>
+              </Navbar.Brand>
+            </Container>
+          </Navbar>
+        </Row>
+        <Row className='justify-content-around mt-3'>
+          {data.map((product) => {
+            return (
+              <Col key={product.id} xl={'auto'} className='p-2'>
+                <Card key={product.id} style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={product.image} style={{ width: '100%', height: 250 }} />
+                  <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>
+                      {product.description}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+          })}
         </Row>
       </Container>
     </>
