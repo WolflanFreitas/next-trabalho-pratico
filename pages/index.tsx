228c94acc421232ab119e33fc3191ca921536f6d
WolflanFreitas <wolflanf@gmail.com>
2022-09-07 17:57:12 -0300

    show individual

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,57 +3,31 @@ import type { NextPage } from 'next'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useFetch } from '../hook/fetchTyped';
-import { Alert } from 'react-bootstrap';
-import { CircleLoader } from 'react-spinners';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'next/image';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import doTruncarStr from '../functions/truncate';
 import { BsFillBasket3Fill } from "react-icons/bs";
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: {
-    rate: number;
-    count: number;
+import Link from 'next/link';
+
+export async function getStaticProps() {
+  const res = await fetch('https://fakestoreapi.com/products?limit=10');
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 10,
   }
 }
 
-const Home: NextPage = () => {
-  const url = 'https://fakestoreapi.com/products?limit=10';
-  const { data, error } = useFetch<Product[]>(url);
+const Home: NextPage = (props: any) => {
 
-  //Será chamado quando houver erro ao chamar a api (indisponibilidade ou chamada incorreta)
-  if (error) {
-    return (
-      <Container fluid>
-        <Row className='mt-5'>
-          <Col xl={12}>
-            <Alert variant='danger'>Não foi possível realizar a conexão na api: <a href={url}>{url}</a></Alert>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+  const data = props.data;
 
-  //Ação que será executada enquanto os dados estão sendo carregados da API
-  if (!data) {
-    return (
-      <Container fluid>
-        <Row className='justify-content-center align-content-center vh-100'>
-          <Col xl={'auto'}>
-            <CircleLoader color={'fuchsia'} loading={true} size={150} />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+
 
   return (
     <>
@@ -61,23 +35,24 @@ const Home: NextPage = () => {
         <Row>
           <Navbar bg="dark" variant="dark">
             <Container>
-              <Navbar.Brand href="#home">
+              <Navbar.Brand href="/">
                 <Image src={'/cpu.png'} alt="logo" width={30} height={30} className={"d-inline-block align-top"} />
-                <span className={"d-inline-block align-top"}>Lojão do Hardware</span>
+                <span className={"d-inline-block align-top"}>Lojão dos importados</span>
               </Navbar.Brand>
             </Container>
           </Navbar>
         </Row>
         <Row className='justify-content-around mt-3'>
-          {data.map((product) => {
+          {data.map((product: any) => {
             return (
               <Col key={product.id} xl={'auto'} className='p-2'>
                 <Card key={product.id} style={{ width: '18rem' }}>
                   <Card.Img variant="top" src={product.image} style={{ width: '100%', height: 250 }} />
                   <Card.Body>
                     <Card.Title>{doTruncarStr(product.title, 20)}</Card.Title>
-
-                    <Button variant="primary">Veja mais <BsFillBasket3Fill /></Button>
+                    <Link href={`products/${product.id}`}>
+                      <Button variant="primary">Veja mais <BsFillBasket3Fill /></Button>
+                    </Link>
                   </Card.Body>
                 </Card>
               </Col>
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,4 +1,15 @@
 import type { NextPage } from 'next'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Spinner from 'react-bootstrap/Spinner';
+import { Alert } from 'react-bootstrap';
+import Image from 'next/image';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 interface Product {
     id: number;
@@ -13,10 +24,76 @@ interface Product {
     }
 }
 
-const Products: NextPage = () => {
+export async function getStaticPaths() {
+
+    const res = await fetch('https://fakestoreapi.com/products?top=10');
+
+    const products = await res.json();
+
+    const paths = products.map((product: any) => ({
+        params: { id: product.id.toString() }
+    }))
+
+    return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }: { params: Product }) {
+    const res = await fetch(`https://fakestoreapi.com/products/${params.id}`);
+
+    const product = await res.json();
+
+    return { props: { product } }
+}
+
+const Products = (props: any) => {
+
+    const product = props.product;
+
     return (
         <>
-            <p>aqui</p>
+            <Container fluid>
+                <Row>
+                    <Navbar bg="dark" variant="dark">
+                        <Container>
+                            <Navbar.Brand href="/">
+                                <Image src={'/cpu.png'} alt="logo" width={30} height={30} className={"d-inline-block align-top"} />
+                                <span className={"d-inline-block align-top"}>Lojão dos importados</span>
+                            </Navbar.Brand>
+                        </Container>
+                    </Navbar>
+                </Row>
+                <Row className='mt-5 justify-content-center'>
+                    <Col xl={6}>
+                        <Card>
+                            <Card.Header className='bg-primary text-white'>{product.title}</Card.Header>
+                            <Card.Body>
+                                <Row>
+                                    <Col xl={3}>
+                                        <Card.Img variant="top" src={product.image} style={{ width: '100%', height: 250 }} />
+                                    </Col>
+                                    <Col xl={9}>
+                                        <Card.Text style={{ textAlign: 'justify' }}>
+                                            <p>
+                                                <strong>Descrição:</strong> <span>{product.description}</span>
+                                            </p>
+                                            <p>
+                                                <strong>Preço:</strong> <span>{product.price}</span>
+                                            </p>
+                                            <p>
+                                                <strong>Categoria:</strong> <span>{product.category}</span>
+                                            </p>
+                                            <p>
+                                                <strong>Avaliação:</strong> <span>{product.rating.rate}/5</span>
+                                            </p>
+                                        </Card.Text>
+                                        <Button variant="success">Comprar</Button>
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
