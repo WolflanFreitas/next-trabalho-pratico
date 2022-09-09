f3d165d7196ed9d603d6ba9fd4555a45f617c48e
Wolflan Freitas <wolflanf@gmail.com>
2022-09-09 12:53:26 -0300

    config domain fakestroreapi

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['fakestoreapi.com'],
+  },
+  i18n: {
+    locales: ['pt', 'en'],
+    defaultLocale: 'pt',
+  },
 }
 
 module.exports = nextConfig
